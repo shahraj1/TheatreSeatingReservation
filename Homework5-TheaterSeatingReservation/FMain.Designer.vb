43c91ac9899d25297a40a989423d6c06de848385
shahraj1 <shahraj603@gmail.com>
2020-05-08 13:52:08 -0400

    first commit

---
+++ Homework5-TheaterSeatingReservation/FMain.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.msFile = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msFileExit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msSeats = New System.Windows.Forms.ToolStripMenuItem()
+        Me.msSeatsClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lblNumbers = New System.Windows.Forms.Label()
+        Me.pnlSeats = New System.Windows.Forms.Panel()
+        Me.lblSeatsTaken = New System.Windows.Forms.Label()
+        Me.lblSeatsEmpty = New System.Windows.Forms.Label()
+        Me.txtSeatsTaken = New System.Windows.Forms.TextBox()
+        Me.txtSeatsEmpty = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.msFile, Me.msSeats})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(406, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "mnuMenuStrip"
+        '
+        'msFile
+        '
+        Me.msFile.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.msFileExit})
+        Me.msFile.Name = "msFile"
+        Me.msFile.Size = New System.Drawing.Size(44, 24)
+        Me.msFile.Text = "File"
+        '
+        'msFileExit
+        '
+        Me.msFileExit.Name = "msFileExit"
+        Me.msFileExit.Size = New System.Drawing.Size(152, 24)
+        Me.msFileExit.Text = "Exit"
+        '
+        'msSeats
+        '
+        Me.msSeats.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.msSeatsClear})
+        Me.msSeats.Name = "msSeats"
+        Me.msSeats.Size = New System.Drawing.Size(56, 24)
+        Me.msSeats.Text = "Seats"
+        '
+        'msSeatsClear
+        '
+        Me.msSeatsClear.Name = "msSeatsClear"
+        Me.msSeatsClear.Size = New System.Drawing.Size(152, 24)
+        Me.msSeatsClear.Text = "Clear"
+        '
+        'lblNumbers
+        '
+        Me.lblNumbers.AutoSize = True
+        Me.lblNumbers.Location = New System.Drawing.Point(43, 56)
+        Me.lblNumbers.Name = "lblNumbers"
+        Me.lblNumbers.Size = New System.Drawing.Size(204, 17)
+        Me.lblNumbers.TabIndex = 14
+        Me.lblNumbers.Text = "1   2   3   4   5   6   7   8   9   10"
+        '
+        'pnlSeats
+        '
+        Me.pnlSeats.AutoScroll = True
+        Me.pnlSeats.Location = New System.Drawing.Point(23, 77)
+        Me.pnlSeats.Margin = New System.Windows.Forms.Padding(4)
+        Me.pnlSeats.Name = "pnlSeats"
+        Me.pnlSeats.Size = New System.Drawing.Size(260, 587)
+        Me.pnlSeats.TabIndex = 15
+        '
+        'lblSeatsTaken
+        '
+        Me.lblSeatsTaken.AutoSize = True
+        Me.lblSeatsTaken.Location = New System.Drawing.Point(302, 107)
+        Me.lblSeatsTaken.Name = "lblSeatsTaken"
+        Me.lblSeatsTaken.Size = New System.Drawing.Size(92, 17)
+        Me.lblSeatsTaken.TabIndex = 16
+        Me.lblSeatsTaken.Text = "Seats Taken:"
+        '
+        'lblSeatsEmpty
+        '
+        Me.lblSeatsEmpty.AutoSize = True
+        Me.lblSeatsEmpty.Location = New System.Drawing.Point(303, 167)
+        Me.lblSeatsEmpty.Name = "lblSeatsEmpty"
+        Me.lblSeatsEmpty.Size = New System.Drawing.Size(91, 17)
+        Me.lblSeatsEmpty.TabIndex = 16
+        Me.lblSeatsEmpty.Text = "Seats Empty:"
+        '
+        'txtSeatsTaken
+        '
+        Me.txtSeatsTaken.Enabled = False
+        Me.txtSeatsTaken.Location = New System.Drawing.Point(305, 127)
+        Me.txtSeatsTaken.Name = "txtSeatsTaken"
+        Me.txtSeatsTaken.Size = New System.Drawing.Size(87, 22)
+        Me.txtSeatsTaken.TabIndex = 17
+        '
+        'txtSeatsEmpty
+        '
+        Me.txtSeatsEmpty.Enabled = False
+        Me.txtSeatsEmpty.Location = New System.Drawing.Point(306, 187)
+        Me.txtSeatsEmpty.Name = "txtSeatsEmpty"
+        Me.txtSeatsEmpty.Size = New System.Drawing.Size(87, 22)
+        Me.txtSeatsEmpty.TabIndex = 17
+        '
+        'FMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(406, 693)
+        Me.Controls.Add(Me.txtSeatsEmpty)
+        Me.Controls.Add(Me.txtSeatsTaken)
+        Me.Controls.Add(Me.lblSeatsEmpty)
+        Me.Controls.Add(Me.lblSeatsTaken)
+        Me.Controls.Add(Me.pnlSeats)
+        Me.Controls.Add(Me.lblNumbers)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FMain"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents msFile As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents msFileExit As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents msSeats As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents msSeatsClear As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents lblNumbers As System.Windows.Forms.Label
+    Friend WithEvents pnlSeats As System.Windows.Forms.Panel
+    Friend WithEvents lblSeatsTaken As System.Windows.Forms.Label
+    Friend WithEvents lblSeatsEmpty As System.Windows.Forms.Label
+    Friend WithEvents txtSeatsTaken As System.Windows.Forms.TextBox
+    Friend WithEvents txtSeatsEmpty As System.Windows.Forms.TextBox
+
+End Class
---
+++ Homework5-TheaterSeatingReservation/FMain.vb
@@ -0,0 +1,180 @@
+﻿'--------------------------------------------------
+' Name : Ruja Rajbhandari   
+' Class: IT-102-001
+' Abstract: Homework 5- Theatre Seating Reservation
+'--------------------------------------------------
+
+Public Class FMain
+
+    'Checkbox Height and Width
+    Private Const f_intCheckboxHeight As Integer = 15
+    Private Const f_intCheckboxWidth As Integer = 15
+
+
+    Private Const f_intSeatColumns As Integer = 10
+    Private Const f_intSeatRows As Integer = 25
+
+    'Variable to store Checkbox that is created
+    Private chkTheaterSeats(,) As CheckBox
+
+    'store the number of occupied and not occupied seats
+    Private m_intTotalFilled As Integer
+    Private m_intTotalEmpty As Integer
+
+    ' pntCurrent is used to create a Point structure.
+    Private pntCurrent As System.Drawing.Point
+
+    Private Sub FMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+
+            BuildSeats()
+
+        Catch ex As Exception
+
+            'Error Message
+            MessageBox.Show("Cannot load Seats")
+
+        End Try
+
+    End Sub
+
+
+
+    Public Sub BuildSeats()
+
+        ReDim chkTheaterSeats(f_intSeatColumns, f_intSeatRows)
+
+        Dim pintCurrentColumn As Integer
+        Dim pintCurrentRow As Integer
+
+        ' Define the total number of seats.
+        m_intTotalEmpty = f_intSeatColumns * f_intSeatRows
+
+        txtSeatsTaken.Text = m_intTotalFilled.ToString()
+        txtSeatsEmpty.Text = m_intTotalEmpty.ToString()
+
+        'Columns
+        For pintCurrentColumn = 0 To f_intSeatColumns - 1
+
+            'Rows
+            For pintCurrentRow = 0 To f_intSeatRows - 1
+
+                pntCurrent = New Point((pintCurrentColumn + 1) * f_intCheckboxWidth, (pintCurrentRow + 1) * f_intCheckboxHeight)
+
+                'Create Checkbox in that point/place
+                CreateCheckBox(pintCurrentColumn, pintCurrentRow, pntCurrent)
+
+                'Create row at the end of that row
+                CreateRowLabel(pintCurrentRow, (pintCurrentRow + 1) * f_intCheckboxHeight)
+
+            Next
+
+        Next
+
+
+    End Sub
+    Private Sub CreateCheckBox(ByVal pintSeatCurrent As Integer, ByVal pintCurrentRow As Integer, ByVal pntCurrent As Point)
+
+
+        ' Create an instance of Checkbox class
+        chkTheaterSeats(pintSeatCurrent, pintCurrentRow) = New CheckBox()
+
+        'Make it visible
+        chkTheaterSeats(pintSeatCurrent, pintCurrentRow).Visible = True
+
+        'Define the size of the checkbox
+        chkTheaterSeats(pintSeatCurrent, pintCurrentRow).Size = New System.Drawing.Size(f_intCheckboxWidth, f_intCheckboxHeight)
+
+        'Define the position
+        chkTheaterSeats(pintSeatCurrent, pintCurrentRow).Location = pntCurrent
+
+        'Add the event handler
+        AddHandler chkTheaterSeats(pintSeatCurrent, pintCurrentRow).CheckedChanged, AddressOf CheckedChangedTheaterSeats
+
+        Me.pnlSeats.Controls.Add(chkTheaterSeats(pintSeatCurrent, pintCurrentRow))
+
+    End Sub
+
+
+
+    Private Sub CreateRowLabel(ByVal pintCurrentRow As Integer, pintTop As Integer)
+
+        Dim lblRows As Label = New Label()
+
+        lblRows.Left = 165
+        lblRows.Top = pintTop
+        lblRows.Height = 15
+        lblRows.Width = 25
+        'Create a name for the row. Initialize with 1 
+        lblRows.Text = (pintCurrentRow + 1).ToString()
+
+        'Add control to the panel
+        pnlSeats.Controls.Add(lblRows)
+
+    End Sub
+
+
+    Private Sub CheckedChangedTheaterSeats(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+        Dim chkCurrent As System.Windows.Forms.CheckBox
+
+        '
+        chkCurrent = CType(sender, System.Windows.Forms.CheckBox)
+
+        Select Case chkCurrent.Checked
+
+            Case True
+
+                'yes?checked?
+                m_intTotalFilled += 1
+                m_intTotalEmpty -= 1
+
+            Case False
+
+                'Not checked?
+                m_intTotalFilled -= 1
+                m_intTotalEmpty += 1
+
+        End Select
+
+        'Display the result
+        txtSeatsTaken.Text = m_intTotalFilled.ToString()
+        txtSeatsEmpty.Text = m_intTotalEmpty.ToString()
+
+    End Sub
+
+
+    Private Sub msFileExit_Click(sender As Object, e As EventArgs) Handles msFileExit.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub msSeatsClear_Click(sender As Object, e As EventArgs) Handles msSeatsClear.Click
+
+        Dim ctlCurrent As Control
+        Dim chkCurrent As CheckBox
+
+        ' The For Each loop enumerates the Controls collection for the 
+        ' Panel rather than the form.
+        For Each ctlCurrent In pnlSeats.Controls
+
+            'Check if it is a checkbox
+            If TypeOf (ctlCurrent) Is System.Windows.Forms.CheckBox Then
+
+                'Yes? Take the checkbox and clear the mark on it
+                chkCurrent = CType(ctlCurrent, System.Windows.Forms.CheckBox)
+                chkCurrent.Checked = False
+
+            End If
+
+        Next
+
+        'Clear the textboxes
+        txtSeatsTaken.Text = ""
+        txtSeatsEmpty.Text = ""
+
+    End Sub
+
+
+End Class
